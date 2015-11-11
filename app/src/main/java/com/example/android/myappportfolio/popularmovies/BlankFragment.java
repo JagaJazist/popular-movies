47811b943f9acb2e7f68beb1d27ba2a5bf83cfc1
postnov <postnov88@gmail.com>
2015-11-11 00:54:29 +0000

    try add grid view

--- app/src/main/java/com/example/android/myappportfolio/popularmovies/BlankFragment.java
+++ app/src/main/java/com/example/android/myappportfolio/popularmovies/BlankFragment.java
@@ -1,23 +1,17 @@
 package com.example.android.myappportfolio.popularmovies;
 
 import android.app.Activity;
+import android.app.ListFragment;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link BlankFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link BlankFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class BlankFragment extends Fragment {
+public class BlankFragment extends ListFragment {
 
     // TODO: Rename and change types and number of parameters
     public static BlankFragment newInstance() {
@@ -32,6 +26,12 @@ public class BlankFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2" };
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
     }
 
     @Override
