c39162d699e06bc8afef14cf9e9e40e3d64e0c88
postnov <postnov88@gmail.com>
2015-11-12 21:19:56 +0000

    add api key to build config

---
+++ app/src/main/java/com/example/android/myappportfolio/popularmovies/MovieApiHelper.java
@@ -0,0 +1,10 @@
+package com.example.android.myappportfolio.popularmovies;
+
+/**
+ * Created by rcc on 12.11.15.
+ */
+public class MovieApiHelper {
+    private static String KEY = BuildConfig.MOVIE_DB_MAP_API_KEY;
+
+
+}
