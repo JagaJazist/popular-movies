55bcde4dca801c99762e27542567eb47e3e9de33
Aleksandr Postnov <apostnov@badoos-MacBook-Pro-269.local>
2015-11-24 13:20:54 +0000

    use ButterKnife + move Fetch movies class to a separate file

--- app/src/main/java/com/example/android/myappportfolio/popularmovies/MovieDetailsFragment.java
+++ app/src/main/java/com/example/android/myappportfolio/popularmovies/MovieDetailsFragment.java
@@ -12,8 +12,18 @@ import android.widget.TextView;
 import com.example.android.myappportfolio.popularmovies.Models.Movie;
 import com.squareup.picasso.Picasso;
 
+ import butterknife.Bind;
+ import butterknife.ButterKnife;
+
 
 public class MovieDetailsFragment extends Fragment {
+
+    @Bind(R.id.details_title) TextView title;
+    @Bind(R.id.movie_image) ImageView imageView;
+    @Bind(R.id.release_date) TextView releaseDate;
+    @Bind(R.id.average_vote) TextView averageVote;
+    @Bind(R.id.summary) TextView summary;
+
 
     public MovieDetailsFragment() {
     }
@@ -26,24 +36,17 @@ public class MovieDetailsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_details, container, false);
+        ButterKnife.bind(this, view);
         Movie movie = getActivity().getIntent().getParcelableExtra(Intent.EXTRA_TEXT);
 
-        TextView title = (TextView) view.findViewById(R.id.details_title);
         title.setText(movie.title);
-
-        ImageView imageView = (ImageView) view.findViewById(R.id.movie_image);
         Picasso.with(getActivity()).load(movie.posterUrl)
                 .error(R.drawable.honeycomb)
                 .into(imageView);
-
-        TextView releaseDate = (TextView) view.findViewById(R.id.release_date);
         releaseDate.setText(movie.releaseDate);
-
-        TextView averageVote = (TextView) view.findViewById(R.id.average_vote);
         averageVote.setText(movie.voteAverage + "/10");
-
-        TextView summary = (TextView) view.findViewById(R.id.summary);
         summary.setText(movie.plotSynopsis);
 
         return view;
--- app/src/main/java/com/example/android/myappportfolio/popularmovies/MoviesGridFragment.java
+++ app/src/main/java/com/example/android/myappportfolio/popularmovies/MoviesGridFragment.java
@@ -1,11 +1,8 @@
 package com.example.android.myappportfolio.popularmovies;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -17,12 +14,6 @@ import android.widget.GridView;
 
 import com.example.android.myappportfolio.popularmovies.Models.Movie;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 
 
@@ -30,7 +21,7 @@ public class MoviesGridFragment extends Fragment {
 
     private static final String LOG_TAG = MoviesGridFragment.class.getSimpleName();
 
-    private MovieGridAdapter<Movie> movieGridAdapter;
+    MovieGridAdapter<Movie> movieGridAdapter;
     private ArrayList<Movie> moviesList;
 
     public MoviesGridFragment() {
@@ -63,10 +54,10 @@ public class MoviesGridFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.popular:
-                new FetchMovies().execute(MoviesSortingType.POPULAR);
+                new FetchMovies(this).execute(MoviesSortingType.POPULAR);
                 return true;
             case R.id.rating:
-                new FetchMovies().execute(MoviesSortingType.RATING);
+                new FetchMovies(this).execute(MoviesSortingType.RATING);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -76,7 +67,7 @@ public class MoviesGridFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        new FetchMovies().execute();
+        new FetchMovies(this).execute();
 
         View rootView = inflater.inflate(R.layout.fragment_movies_grid, container, false);
 
@@ -94,101 +85,4 @@ public class MoviesGridFragment extends Fragment {
     }
 
 
-    public class FetchMovies extends AsyncTask<MoviesSortingType, Void, Movie[]> {
-        private static final String API_KEY = BuildConfig.MOVIE_DB_MAP_API_KEY;
-        private final String LOG_TAG = FetchMovies.class.getSimpleName();
-
-
-        @Override
-        protected Movie[] doInBackground(MoviesSortingType... params) {
-            // These two need to be declared outside the try/catch
-            // so that they can be closed in the finally block.
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-
-            // Will contain the raw JSON response as a string.
-            String moviesJson;
-            String sortingParameter = "popularity.desc";
-
-            if (params.length > 0) {
-                switch (params[0]) {
-                    case POPULAR:
-                        sortingParameter = "popularity.desc";
-                        break;
-                    case RATING:
-                        sortingParameter = "vote_average.desc";
-                        break;
-                }
-            }
-
-            Uri.Builder builder = new Uri.Builder();
-            builder.scheme("http")
-                    .authority("api.themoviedb.org")
-                    .appendPath("3")
-                    .appendPath("discover")
-                    .appendPath("movie")
-                    .appendQueryParameter("sort_by", sortingParameter)
-                    .appendQueryParameter("api_key", API_KEY)
-                    .fragment("section-name");
-            String uri = builder.build().toString();
-
-            try {
-                URL url = new URL(uri);
-
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    buffer.append(line + "\n");
-                }
-
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return null;
-                }
-                moviesJson = buffer.toString();
-
-                Movie[] result = ParseMovieData.getMoviesFromJson(moviesJson);
-
-                return result;
-
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error ", e);
-                return null;
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e(LOG_TAG, "Error closing stream", e);
-                    }
-                }
-            }
-        }
-
-        @Override
-        public void onPostExecute(Movie[] result) {
-            super.onPostExecute(result);
-            movieGridAdapter.clear();
-            if (result != null) {
-                for (Movie movie : result) {
-                    movieGridAdapter.add(movie);
-                }
-            }
-        }
-    }
 }
